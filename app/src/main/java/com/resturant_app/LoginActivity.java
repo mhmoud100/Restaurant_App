632b3e33700189bad4ebf6f3e3f7f04a31fc6266
mhmoud <mhmoudsoliman41@outlook.com>
2020-12-22 19:38:02 +0200

    First Commit

--- app/src/main/java/com/resturant_app/LoginActivity.java
+++ app/src/main/java/com/resturant_app/LoginActivity.java
@@ -3,12 +3,16 @@ package com.resturant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class LoginActivity extends AppCompatActivity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
     }
 }
