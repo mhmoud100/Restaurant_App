8b3f797168a6de0de5369d26c9977c48b921ec6b
Youssef <youssefabdullah235@gmail.com>
2020-12-24 22:15:49 +0200

    SinglePage

---
+++ app/src/main/java/com/resturant_app/SinglePageApp.java
@@ -0,0 +1,14 @@
+package com.resturant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinglePageApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_page_app);
+    }
+}
