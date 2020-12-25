00531130c01219dcd966019815a269cbd51554e2
Abdelrhman Mohamed Ezz-Eldin <geniusgen27300@gmail.com>
2020-12-25 03:02:17 +0200

    3mlt el item view na2s azbt alwan el zrayr bs

---
+++ app/src/main/java/com/resturant_app/activity_item_view.java
@@ -0,0 +1,14 @@
+package com.resturant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_item_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_view);
+    }
+}
