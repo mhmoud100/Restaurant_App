24a7b3d8f4829c3e81e190964a33c5515c7dedc0
Youssef <youssefabdullah235@gmail.com>
2020-12-26 16:59:53 +0200

    SinglePage Connect prodect

--- app/src/main/java/com/resturant_app/ProductAdapter.java
+++ app/src/main/java/com/resturant_app/ProductAdapter.java
@@ -1,13 +1,16 @@
 package com.resturant_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -56,6 +59,18 @@ public class ProductAdapter extends BaseAdapter {
         title.setText(item.getTitle());
         desc.setText(item.getDesc());
         price.setText(item.getPrice());
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),SinglePageApp.class);
+                intent.putExtra("title",item.getTitle());
+                intent.putExtra("Price",item.getPrice());
+                intent.putExtra("Details",item.getDetails());
+                context.startActivity(intent);
+
+
+            }
+        });
         StorageReference ref = FirebaseStorage.getInstance().getReference();
         ref.child("Products").child(item.getPid()).child("Product_Image").getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
             @Override
--- app/src/main/java/com/resturant_app/SinglePageApp.java
+++ app/src/main/java/com/resturant_app/SinglePageApp.java
@@ -3,12 +3,60 @@ package com.resturant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SinglePageApp extends AppCompatActivity {
-
+TextView nameOfitem , Price ,des;
+Button plus , btnNummer ,mins ;
+int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_page_app);
+        String title = getIntent().getStringExtra("title");
+        String price = getIntent().getStringExtra("Price");
+        String Des  = getIntent().getStringExtra("Details");
+        x = 1;
+
+        nameOfitem =findViewById(R.id.jotext);
+        Price = findViewById(R.id.jocost);
+        btnNummer = findViewById(R.id.jobtnNumber);
+        plus = findViewById(R.id.jobtnplus);
+        mins = findViewById(R.id.jobtnmis);
+        des = findViewById(R.id.joDes);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                x++;
+                btnNummer.setText(x+"");
+                int temp = Integer.parseInt(price) * x;
+
+                Price.setText("$" +temp);
+
+            }
+        });
+        mins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(x>0){
+                    x--;
+                    btnNummer.setText(x+"");
+                    int temp = Integer.parseInt(price) * x;
+                    Price.setText("$" +temp);
+                }
+
+            }
+        });
+
+
+
+        nameOfitem.setText(title);
+        Price.setText("$" +price);
+        des.setText(Des);
     }
+
 }
