cea81db23b4d0ceead2eae045149937027276e4b
mhmoud <mhmoudsoliman41@outlook.com>
2020-12-27 23:34:51 +0200

    profile commit

--- app/src/main/java/com/resturant_app/ProfileFragment.java
+++ app/src/main/java/com/resturant_app/ProfileFragment.java
@@ -7,15 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
-
+TextView userName , userEmail , userPhone;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        userName = view.findViewById(R.id.joUserName1);
+        userEmail = view.findViewById(R.id.joEmail);
+        userPhone = view.findViewById(R.id.joPhone);
+
+        userName.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+        userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        userPhone.setText(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
         return view;
     }
 }
